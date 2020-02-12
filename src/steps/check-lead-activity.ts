35e32b0b4d8d0790a279d7e6a358cbac6f5796e8
Andrei <apanaligan@you-source.com>
2020-02-12 16:56:38 +0800

    Added expected records to check activity steps

--- src/steps/check-lead-activity.ts
+++ src/steps/check-lead-activity.ts
@@ -29,6 +29,29 @@ export class CheckLeadActivityStep extends BaseStep implements StepInterface {
     description: 'Represents additional parameters that should be used to validate an activity. The key in the object represents an attribute name and the value represents the expected value',
     optionality: FieldDefinition.Optionality.OPTIONAL,
   }];
+  // const headers = { id: 'ID', leadId: 'Lead ID', activityDate: 'Activity Date', activityTypeId: 'Activity Type ID' };
+  protected expectedRecords: ExpectedRecord[] = [{
+    id: 'matchedActivities',
+    type: RecordDefinition.Type.TABLE,
+    fields: [{
+      field: 'id',
+      type: FieldDefinition.Type.NUMERIC,
+      description: "Activity's Marketo ID",
+    }, {
+      field: 'leadId',
+      type: FieldDefinition.Type.NUMERIC,
+      description: "Lead's Marketo ID",
+    }, {
+      field: 'activityDate',
+      type: FieldDefinition.Type.DATETIME,
+      description: "Activity's Date",
+    }, {
+      field: 'activityTypeId',
+      type: FieldDefinition.Type.NUMERIC,
+      description: "Activity Type's ID",
+    }],
+    dynamicFields: true,
+  }];
 
   protected expectedRecords: ExpectedRecord[] = [{
     id: 'matchedActivities',
