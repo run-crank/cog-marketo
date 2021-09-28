5efef309524f1d04fd4ff76eb770ca6f047b8e1a
Austin Elwell <austinelwell99@gmail.com>
2021-09-28 14:22:04 +0200

    Added redis to grpc server, not being used yet

--- src/core/grpc-server.ts
+++ src/core/grpc-server.ts
@@ -2,11 +2,13 @@ import * as grpc from 'grpc';
 import { CogServiceService as CogService } from '../proto/cog_grpc_pb';
 import { Cog } from './cog';
 import { ClientWrapper } from '../client/client-wrapper';
+import * as redis from 'redis';
 
 const server = new grpc.Server();
 const port = process.env.PORT || 28866;
 const host = process.env.HOST || '0.0.0.0';
 let credentials: grpc.ServerCredentials;
+let redisClient: {};
 
 if (process.env.USE_SSL) {
   credentials = grpc.ServerCredentials.createSsl(
@@ -20,6 +22,13 @@ if (process.env.USE_SSL) {
   credentials = grpc.ServerCredentials.createInsecure();
 }
 
+if (process.env.REDIS_URL) {
+  // Hosted environment redis connection.
+  redisClient = redis.createClient(process.env.REDIS_URL);
+} else {
+  // Local client (requires no auth details).
+  redisClient = redis.createClient();
+}
 server.addService(CogService, new Cog(ClientWrapper));
 server.bind(`${host}:${port}`, credentials);
 server.start();
