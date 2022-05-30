85c361fbd792799756cc1d189a504671e0a3de45
saludangelito <asalud@you-source.com>
2022-05-30 15:22:37 +0800

    Implemented Check Program Member fields

---
+++ src/steps/program-members/program-member-field-equals.ts
@@ -0,0 +1,132 @@
+/*tslint:disable:no-else-after-return*/
+
+import { BaseStep, Field, StepInterface, ExpectedRecord } from '../../core/base-step';
+import { Step, FieldDefinition, StepDefinition, RecordDefinition } from '../../proto/cog_pb';
+import * as util from '@run-crank/utilities';
+import { baseOperators } from '../../client/constants/operators';
+import { isNullOrUndefined } from 'util';
+
+export class ProgramMemberFieldEqualsStep extends BaseStep implements StepInterface {
+
+  protected stepName: string = 'Check a field on a Marketo Program Member';
+  // tslint:disable-next-line:max-line-length
+  protected stepExpression: string = 'the (?<field>[a-zA-Z0-9_-]+) field on marketo member (?<email>.+) from program (?<programName>.+) should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain|match|not match) ?(?<expectation>.+)?';
+  protected stepType: StepDefinition.Type = StepDefinition.Type.VALIDATION;
+  protected expectedFields: Field[] = [{
+    field: 'programName',
+    type: FieldDefinition.Type.STRING,
+    description: "Program's Name",
+  }, {
+    field: 'email',
+    type: FieldDefinition.Type.EMAIL,
+    description: "Lead's email",
+  }, {
+    field: 'field',
+    type: FieldDefinition.Type.STRING,
+    description: 'Field name to check',
+  }, {
+    field: 'operator',
+    type: FieldDefinition.Type.STRING,
+    optionality: FieldDefinition.Optionality.OPTIONAL,
+    description: 'Check Logic (be, not be, contain, not contain, be greater than, be less than, be set, not be set, be one of, or not be one of)',
+  }, {
+    field: 'expectation',
+    type: FieldDefinition.Type.ANYSCALAR,
+    optionality: FieldDefinition.Optionality.OPTIONAL,
+    description: 'Expected field value',
+  }, {
+    field: 'partitionId',
+    type: FieldDefinition.Type.NUMERIC,
+    optionality: FieldDefinition.Optionality.OPTIONAL,
+    description: 'ID of partition lead belongs to',
+    help: 'Only necessary to provide if Marketo has been configured to allow duplicate leads by email.',
+  }];
+  protected expectedRecords: ExpectedRecord[] = [{
+    id: 'programMember',
+    type: RecordDefinition.Type.KEYVALUE,
+    fields: [{
+      field: 'leadId',
+      type: FieldDefinition.Type.STRING,
+      description: "Lead's Marketo ID",
+    }, {
+      field: 'programId',
+      type: FieldDefinition.Type.STRING,
+      description: "Program's Marketo ID",
+    }],
+    dynamicFields: false,
+  }];
+
+  async executeStep(step: Step) {
+    const stepData: any = step.getData() ? step.getData().toJavaScript() : {};
+    const expectedValue = stepData.expectation;
+    const programName = stepData.programName;
+    const email = stepData.email;
+    const operator: string = stepData.operator || 'be';
+    const partitionId: number = stepData.partitionId ? parseFloat(stepData.partitionId) : null;
+    const field = stepData.field;
+
+    if (isNullOrUndefined(expectedValue) && !(operator == 'be set' || operator == 'not be set')) {
+      return this.error("The operator '%s' requires an expected value. Please provide one.", [operator]);
+    }
+
+    try {
+      // Check if lead exists and also get the id
+      const lead: any = await this.client.findLeadByEmail(email, null, partitionId);
+      if (lead.result.length === 0) {
+        return this.error('Lead with email %s does not exist%s', [
+          email,
+          partitionId ? ` in partition ${partitionId}` : '',
+        ]);
+      }
+
+      // Check if program exists and also get the id
+      const program: any = await this.client.findProgramsByName(programName);
+      if (program.result.length === 0) {
+        return this.error('Program with name %s does not exist', [
+          programName,
+        ]);
+      }
+
+      // Get all available fields to be included when getting program member
+      const memberFields: any = (await this.client.getProgramMembersFields()).result[0]['fields'].map(f => f.name);
+
+      const data: any = await this.client.getProgramMembersByProgramId(program.result[0].id, 'leadId', lead.result[0].id, memberFields);
+
+      if (data.success && data.result && data.result[0] && data.result[0].hasOwnProperty(field)) {
+        let result;
+        result = this.assert(operator, data.result[0][field], expectedValue, field);
+
+        return result.valid ? this.pass(result.message, [], [this.createRecord(data.result[0])])
+          : this.fail(result.message, [], [this.createRecord(data.result[0])]);
+
+      } else {
+        if (data.result && data.result[0] && !data.result[0][field]) {
+          return this.fail(
+            'Found the %s program member, but there was no %s field.',
+            [email, field],
+            [this.createRecord(data.result[0])],
+          );
+        } else {
+          return this.fail("Couldn't find a program member associated with %s%s", [
+            email,
+            partitionId ? ` in partition ${partitionId}` : '',
+          ]);
+        }
+      }
+    } catch (e) {
+      if (e instanceof util.UnknownOperatorError) {
+        return this.error('%s Please provide one of: %s', [e.message, baseOperators.join(', ')]);
+      }
+      if (e instanceof util.InvalidOperandError) {
+        return this.error(e.message);
+      }
+      return this.error('There was an error during validation of program field: %s', [e.message]);
+    }
+  }
+
+  createRecord(programMember: Record<string, any>) {
+    return this.keyValue('programMember', 'Checked Program Member', programMember);
+  }
+}
+
+export { ProgramMemberFieldEqualsStep as Step };
---
+++ test/steps/program-members/program-field-equals.ts
@@ -0,0 +1,256 @@
+import { Struct } from 'google-protobuf/google/protobuf/struct_pb';
+import * as chai from 'chai';
+import { default as sinon } from 'ts-sinon';
+import * as sinonChai from 'sinon-chai';
+import 'mocha';
+
+import { Step as ProtoStep, StepDefinition, FieldDefinition, RunStepResponse } from '../../../src/proto/cog_pb';
+import { Step } from '../../../src/steps/program-members/program-member-field-equals';
+
+chai.use(sinonChai);
+
+describe('ProgramMemberFieldEqualsStep', () => {
+  const expect = chai.expect;
+  let protoStep: ProtoStep;
+  let stepUnderTest: Step;
+  let clientWrapperStub: any;
+
+  beforeEach(() => {
+    protoStep = new ProtoStep();
+    clientWrapperStub = sinon.stub();
+    clientWrapperStub.findLeadByEmail = sinon.stub();
+    clientWrapperStub.findProgramsByName = sinon.stub();
+    clientWrapperStub.getProgramMembersFields = sinon.stub();
+    clientWrapperStub.getProgramMembersByProgramId = sinon.stub();
+    stepUnderTest = new Step(clientWrapperStub);
+
+    clientWrapperStub.findLeadByEmail.returns(Promise.resolve({
+      success: true,
+      result: [
+        {id: 123123},
+      ],
+    }));
+
+    clientWrapperStub.findProgramsByName.returns(Promise.resolve({
+      success: true,
+      result: [
+        {id: 123123, name: 'anyProgram'},
+      ],
+    }));
+
+    clientWrapperStub.getProgramMembersFields.returns(Promise.resolve({
+      success: true,
+      result: [
+        {name: 'anyFieldName', fields: [{name: 'anyField'}]},
+      ],
+    }));
+
+    clientWrapperStub.getProgramMembersByProgramId.returns(Promise.resolve({
+      success: true,
+      result: [
+        {leadId: 123123, programId: 123123, anyField: 'anyValue'},
+      ],
+    }));
+  });
+
+  it('should return expected step metadata', () => {
+    const stepDef: StepDefinition = stepUnderTest.getDefinition();
+    expect(stepDef.getStepId()).to.equal('ProgramMemberFieldEqualsStep');
+    expect(stepDef.getName()).to.equal('Check a field on a Marketo Program Member');
+    expect(stepDef.getExpression()).to.equal('the (?<field>[a-zA-Z0-9_-]+) field on marketo member (?<email>.+) from program (?<programName>.+) should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain|match|not match) ?(?<expectation>.+)?');
+    expect(stepDef.getType()).to.equal(StepDefinition.Type.VALIDATION);
+  });
+
+  it('should return expected step fields', () => {
+    const stepDef: StepDefinition = stepUnderTest.getDefinition();
+    const fields: any[] = stepDef.getExpectedFieldsList().map((field: FieldDefinition) => {
+      return field.toObject();
+    });
+
+    // programName field
+    expect(fields[0].key).to.equal('programName');
+    expect(fields[0].optionality).to.equal(FieldDefinition.Optionality.REQUIRED);
+    expect(fields[0].type).to.equal(FieldDefinition.Type.STRING);
+
+    // lead field
+    expect(fields[1].key).to.equal('email');
+    expect(fields[1].optionality).to.equal(FieldDefinition.Optionality.REQUIRED);
+    expect(fields[1].type).to.equal(FieldDefinition.Type.EMAIL);
+
+    // Field Name field
+    expect(fields[2].key).to.equal('field');
+    expect(fields[2].optionality).to.equal(FieldDefinition.Optionality.REQUIRED);
+    expect(fields[2].type).to.equal(FieldDefinition.Type.STRING);
+
+    // Operator field
+    expect(fields[3].key).to.equal('operator');
+    expect(fields[3].optionality).to.equal(FieldDefinition.Optionality.OPTIONAL);
+    expect(fields[3].type).to.equal(FieldDefinition.Type.STRING);
+
+    // Expectation field
+    expect(fields[4].key).to.equal('expectation');
+    expect(fields[4].optionality).to.equal(FieldDefinition.Optionality.OPTIONAL);
+    expect(fields[4].type).to.equal(FieldDefinition.Type.ANYSCALAR);
+
+    // partitionId field
+    expect(fields[5].key).to.equal('partitionId');
+    expect(fields[5].optionality).to.equal(FieldDefinition.Optionality.OPTIONAL);
+    expect(fields[5].type).to.equal(FieldDefinition.Type.NUMERIC);
+  });
+
+  it('should respond with pass', async () => {
+    const expectedField: string = 'anyField';
+    const expectedValue: string = 'anyValue';
+    const programName: string = 'anyProgram';
+    const email: string = 'anyEmail@test.com';
+    protoStep.setData(Struct.fromJavaScript({
+      programName: programName,
+      email: email,
+      field: expectedField,
+      operator: 'be',
+      expectation: expectedValue,
+    }));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.PASSED);
+  });
+
+  it('should respond with an error if the marketo client throws an error', async () => {
+    const expectedField: string = 'anyField';
+    const expectedValue: string = 'anyValue';
+    const programName: string = 'anyProgram';
+    const email: string = 'anyEmail@test.com';
+    protoStep.setData(Struct.fromJavaScript({
+      programName: programName,
+      email: email,
+      field: expectedField,
+      operator: 'be',
+      expectation: expectedValue,
+    }));
+
+    // Cause the client to throw an error, and execute the step.
+    clientWrapperStub.findLeadByEmail.throws('any error');
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+
+  it('should respond with an error if the marketo client did not find lead', async () => {
+    const expectedField: string = 'anyField';
+    const expectedValue: string = 'anyValue';
+    const programName: string = 'anyProgram';
+    const email: string = 'anyEmail@test.com';
+    protoStep.setData(Struct.fromJavaScript({
+      programName: programName,
+      email: email,
+      field: expectedField,
+      operator: 'be',
+      expectation: expectedValue,
+    }));
+
+    // Have the client respond with no programs.
+    clientWrapperStub.findLeadByEmail.resolves({
+      success: true,
+      result: [],
+    });
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+
+  it('should respond with an error if the marketo client did not find program', async () => {
+    const expectedField: string = 'anyField';
+    const expectedValue: string = 'anyValue';
+    const programName: string = 'anyProgram';
+    const email: string = 'anyEmail@test.com';
+    protoStep.setData(Struct.fromJavaScript({
+      programName: programName,
+      email: email,
+      field: expectedField,
+      operator: 'be',
+      expectation: expectedValue,
+    }));
+
+    // Have the client respond with no programs.
+    clientWrapperStub.findProgramsByName.returns(Promise.resolve({
+      success: true,
+      result: [],
+    }));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+
+  it('should respond with an error if the program found does not contain the given field', async () => {
+    const expectedField: string = 'anyField';
+    const expectedValue: string = 'anyValue';
+    const programName: string = 'anyProgram';
+    const email: string = 'anyEmail@test.com';
+    protoStep.setData(Struct.fromJavaScript({
+      programName: programName,
+      email: email,
+      field: expectedField,
+      operator: 'be',
+      expectation: expectedValue,
+    }));
+
+    // Have the client respond with an object not containing the field above.
+    clientWrapperStub.getProgramMembersByProgramId.returns(Promise.resolve({
+      success: true,
+      result: [
+        {leadId: 123123, programId: 123123, anyOtherField: 'anyOtherValue'},
+      ],
+    }));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.FAILED);
+  });
+
+  it('should respond with an unknown util error', async () => {
+    const expectedField: string = 'anyField';
+    const expectedValue: string = 'anyValue';
+    const programName: string = 'anyProgram';
+    const email: string = 'anyEmail@test.com';
+    protoStep.setData(Struct.fromJavaScript({
+      programName: programName,
+      email: email,
+      field: expectedField,
+      operator: 'unknown operator',
+      expectation: expectedValue,
+    }));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getMessageFormat()).to.equal('%s Please provide one of: %s');
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+
+  it('should respond with an invalid operand error', async () => {
+    const expectedField: string = 'anyField';
+    const programName: string = 'anyProgram';
+    const email: string = 'anyEmail@test.com';
+    protoStep.setData(Struct.fromJavaScript({
+      programName: programName,
+      email: email,
+      field: expectedField,
+      operator: 'be greater than',
+      expectation: 'not a number',
+    }));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+
+  it('should respond with an error if expection was not provided when required', async () => {
+    const expectedField: string = 'anyField';
+    const programName: string = 'anyProgram';
+    const email: string = 'anyEmail@test.com';
+    protoStep.setData(Struct.fromJavaScript({
+      programName: programName,
+      email: email,
+      field: expectedField,
+      operator: 'be',
+    }));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+});
